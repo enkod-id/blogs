28ad0836c4aa2c93ee6f82d27291812f85cffbab
enkod-id <natanaeltamto@gmail.com>
2023-09-05 08:55:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.delete('/users/:id', async (req, res) => {
 })
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Running on port http://locahost:${port}`)
